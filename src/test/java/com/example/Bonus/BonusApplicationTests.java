fb31036cc8ce7fa4274ef943e584abe40bc7ae0b
Alvaro Rios <alvaroluisriosgarzon@gmail.com>
2021-12-14 22:20:42 -0500

    SpringBonus

---
+++ src/test/java/com/example/Bonus/BonusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bonus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
